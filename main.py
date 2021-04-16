a0845f352496a14d29258f98de40835b1bc90a8a
Luis Barrera <luisbarrera5662@gmail.com>
2021-04-15 22:15:50 -0600

    HT0.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import math
 
-#variables a utilizar
-
 print("Calculo de indice de masa corporal")
+#variables a utilizar
 peso=float(input("introduce tu peso en kg"))
 altura=float(input("introduce tu altura en metros"))
 imc=round((peso/(altura**2)),2)
+
+
 print("Tu indice de masa corporal es ",imc)
+
+#Luis Angel Barrera Velásquez 202010223
