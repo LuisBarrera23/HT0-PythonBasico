29349bf64678e774589a3f32fc19ab0655199912
Luis Barrera <luisbarrera5662@gmail.com>
2021-04-23 16:39:49 -0600

    HT2 ejercicio 2

--- HT2.py
+++ HT2.py
@@ -12,3 +12,21 @@ def comparar(contraseña):
 
 contraseña1=str(input("Introduzca la contraseña que desea guardar").lower())
 comparar(contraseña1)
+
+#Ejercicio 2
+def asigargrupo(nombre,genero):
+    if (nombre[0].upper() in "ABCDEFGHIJKL") and genero=='F':
+        print("Perteneces al grupo A")
+    elif (nombre[0].upper() in "ÑOPQRSTUVWXYZ") and genero=='M':
+        print("Perteneces al grupo A")
+    elif (nombre[0].upper() in "MNÑOPQRSTUVWXYZ") and genero=='F':
+        print("Perteneces al grupo B")
+    elif (nombre[0].upper() in "ABCDEFGHIJKLMN") and genero=='M':
+        print("Perteneces al grupo B")
+
+nombre=str(input("Por favor ingresa tu nombre "))
+genero=''
+while not (genero=='M' or genero=='F'):
+    genero=str(input("Ingresa tu genero: M para masculino y F para femenino").upper())
+
+asigargrupo(nombre,genero)
